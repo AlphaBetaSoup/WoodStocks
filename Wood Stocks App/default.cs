717227b0330345352ed3d69a0db388d3d6519d18
Allen <allenvawser@hotmail.com>
2021-07-07 19:33:20 +0800

    added default.cs class

---
+++ Wood Stocks App/default.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wood_Stocks_App
+{
+    class @default
+    {
+    }
+}
